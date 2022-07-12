09f72322f0653d75d56e99a5e9d68236cb9e20ba
Bobby Schulz <schulz.bobby@gmail.com>
2022-07-12 08:37:49 -0500

    Updated file location

--- src/Adafruit_SHT31.h
+++ src/Adafruit_SHT31.h
@@ -22,7 +22,7 @@
 
 // #include "Arduino.h"
 #include <Particle.h>
-#include <Adafruit_I2CDevice.h>
+#include <../src/Adafruit_BusIO/Adafruit_I2CDevice.h>
 
 #define SHT31_DEFAULT_ADDR 0x44 /**< SHT31 Default Address */
 #define SHT31_MEAS_HIGHREP_STRETCH                                             \
